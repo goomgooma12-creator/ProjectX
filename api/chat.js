6588130c283d03d1f91fc5400effb3823a232df5
MikeRonald <goomgooma12@gmail.com>
2026-04-14 16:54:16 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -175,8 +175,7 @@ const sessions = {};
 
 export default async function handler(req, res) {
   // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+ 
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
